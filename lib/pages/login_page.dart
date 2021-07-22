018dc873a123e1a4c8add636c7bbf705ae266287
pablotardio <pablo.tv30@gmail.com>
2021-07-22 14:39:45 -0400

    changed to lowercase some titles of login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -126,7 +126,7 @@ class _LoginPageState extends State<LoginPage>
                 ),
                 onPressed: _onSignInButtonPress,
                 child: Text(
-                  'salir',
+                  'Salir',
                   style: TextStyle(
                       color: left,
                       fontSize: 16.0,
@@ -142,7 +142,7 @@ class _LoginPageState extends State<LoginPage>
                 ),
                 onPressed: _onSignUpButtonPress,
                 child: Text(
-                  'nuevo',
+                  'Nuevo',
                   style: TextStyle(
                       color: right,
                       fontSize: 16.0,
