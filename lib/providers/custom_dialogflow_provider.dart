378a2d37d0af42023b7bebb559b1c520a390f60e
pablotardio <pablo.tv30@gmail.com>
2021-08-01 03:13:14 -0400

    created custom dialogflow class from v2

---
+++ lib/providers/custom_dialogflow_provider.dart
@@ -0,0 +1,26 @@
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+
+
+class CustomDialogFlowProvider {
+ AuthGoogle authGoogle;
+   String language;
+
+   CustomDialogFlowProvider({ this.authGoogle  , this.language="en"});
+
+  String _getUrl() {
+    return "https://dialogflow.googleapis.com/v2/projects/${authGoogle.getProjectId}/agent/sessions/${authGoogle.getSessionId}:detectIntent";
+  }
+
+  Future<AIResponse> detectIntent(String queryMessage,String payload) async {
+    var response = await authGoogle.post(_getUrl(),
+        headers: {
+          HttpHeaders.authorizationHeader: "Bearer ${authGoogle.getToken}"
+        },
+        body:
+            "{'queryInput':{'text':{'text':'$queryMessage','language_code':'$language'}}},{'payload': $payload}");
+    return AIResponse(body: json.decode(response.body));
+  }}
