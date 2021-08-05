b7810bf40cc113e7afdd6d4ca8c9031537948124
pablotardio <pablo.tv30@gmail.com>
2021-08-05 16:01:22 -0400

    headers getter created as function

--- lib/providers/env.dart
+++ lib/providers/env.dart
@@ -1,1 +1,11 @@
+import 'package:botybuy/shared_prefs/preferencias_usuarios.dart';
+
 const API_URL = '192.168.1.2:3002';
+getHeaders() {
+  final prefs = new PreferenciasUsuario();
+  final headers = {
+    "Content-Type": "application/json",
+    "authorization": 'bearer ' + prefs.token
+  };
+  return headers;
+}
