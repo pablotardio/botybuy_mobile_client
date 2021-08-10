581a6931942571372ec77f30daaeeeb9ce43de37
pablotardio <pablo.tv30@gmail.com>
2021-08-10 14:17:07 -0400

    created page for catelogo but empty

---
+++ lib/pages/catalogo/catalogo_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class CatalogoPage extends StatefulWidget {
+  CatalogoPage({Key key}) : super(key: key);
+
+  @override
+  _CatalogoPageState createState() => _CatalogoPageState();
+}
+
+class _CatalogoPageState extends State<CatalogoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+    );
+  }
+}
