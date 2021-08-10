ff40c3f2185c2f66be930e715019d290f09c924e
pablotardio <pablo.tv30@gmail.com>
2021-08-10 16:58:09 -0400

    UI Listar Categorias Done

--- lib/pages/catalogo/categorias_catalogo.dart
+++ lib/pages/catalogo/categorias_catalogo.dart
@@ -1,3 +1,4 @@
+import 'package:botybuy/providers/categoria_provider.dart';
 import 'package:botybuy/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
@@ -6,29 +7,43 @@ class CategoriasCatalogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> categories = [
-      {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},
-      {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
-      {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
-      {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
-      {"icon": "assets/icons/Discover.svg", "text": "More"},
-    ];
-    return Padding(
-      padding: EdgeInsets.all(getProportionateScreenWidth(20)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: List.generate(
-          categories.length,
-          (index) => CategoryCard(
-            icon: categories[index]["icon"],
-            text: categories[index]["text"],
-            press: () {},
+    final _categoriaProvider = new CategoriaProvider();
+    return SafeArea(
+        child: FutureBuilder(
+      future: _categoriaProvider.listarCategorias(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Padding(
+          padding: EdgeInsets.all(getProportionateScreenWidth(20)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //snapshot.data es lo que me devuelve mi promise/future
+            children: _listarWidgetsCategoria(context, snapshot.data),
           ),
-        ),
-      ),
-    );
+        );
+      },
+    ));
   }
+}
+
+List<Widget> _listarWidgetsCategoria(
+    BuildContext context, List<dynamic> categoriesData) {
+  Map<String, IconData> categoryIcons = {
+    "Material escolar": Icons.book,
+    "Jugueteria": Icons.toys,
+    "Peluches": Icons.volunteer_activism,
+    "Piscinas": Icons.pool,
+    "Ejercicio": Icons.fitness_center,
+  };
+  return List.generate(
+    categoriesData.length,
+    (index) => CategoryCard(
+      icon: categoryIcons[categoriesData[index]["nombre"]],
+      text: categoriesData[index]["nombre"],
+      press: () {},
+    ),
+  );
 }
 
 class CategoryCard extends StatelessWidget {
@@ -38,8 +53,8 @@ class CategoryCard extends StatelessWidget {
     @required this.text,
     @required this.press,
   }) : super(key: key);
-
-  final String icon, text;
+  final IconData icon;
+  final String text;
   final GestureTapCallback press;
 
   @override
@@ -55,10 +70,10 @@ class CategoryCard extends StatelessWidget {
               height: getProportionateScreenWidth(55),
               width: getProportionateScreenWidth(55),
               decoration: BoxDecoration(
-                color: Color(0xFFFFECDF),
+                color: Theme.of(context).primaryColor.withOpacity(0.7),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Icon(Icons.ac_unit),
+              child: Icon(icon,color:Colors.white,),
             ),
             SizedBox(height: 5),
             Text(text, textAlign: TextAlign.center)
