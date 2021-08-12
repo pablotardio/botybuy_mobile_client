baf0bfb2ddf3c0a19067b7e144f40eeb6f627b8c
pablotardio <pablo.tv30@gmail.com>
2021-08-12 19:29:45 -0400

    added botybuy text to login

--- lib/pages/login_page/sign_in.dart
+++ lib/pages/login_page/sign_in.dart
@@ -181,54 +181,54 @@ class _SignInState extends State<SignIn> {
           //             fontFamily: 'WorkSansMedium'),
           //       )),
           // ),
-          // Padding(
-          //   padding: const EdgeInsets.only(top: 10.0),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: <Widget>[
-          //       Container(
-          //         decoration: const BoxDecoration(
-          //           gradient: LinearGradient(
-          //               colors: <Color>[
-          //                 Colors.white10,
-          //                 Colors.white,
-          //               ],
-          //               begin: FractionalOffset(0.0, 0.0),
-          //               end: FractionalOffset(1.0, 1.0),
-          //               stops: <double>[0.0, 1.0],
-          //               tileMode: TileMode.clamp),
-          //         ),
-          //         width: 100.0,
-          //         height: 1.0,
-          //       ),
-          //       const Padding(
-          //         padding: EdgeInsets.only(left: 15.0, right: 15.0),
-          //         child: Text(
-          //           'O registrarse con',
-          //           style: TextStyle(
-          //               color: Colors.white,
-          //               fontSize: 16.0,
-          //               fontFamily: 'WorkSansMedium'),
-          //         ),
-          //       ),
-          //       Container(
-          //         decoration: const BoxDecoration(
-          //           gradient: LinearGradient(
-          //               colors: <Color>[
-          //                 Colors.white,
-          //                 Colors.white10,
-          //               ],
-          //               begin: FractionalOffset(0.0, 0.0),
-          //               end: FractionalOffset(1.0, 1.0),
-          //               stops: <double>[0.0, 1.0],
-          //               tileMode: TileMode.clamp),
-          //         ),
-          //         width: 100.0,
-          //         height: 1.0,
-          //       ),
-          //     ],
-          //   ),
-          // ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: <Color>[
+                          Colors.white10,
+                          Colors.white,
+                        ],
+                        begin: FractionalOffset(0.0, 0.0),
+                        end: FractionalOffset(1.0, 1.0),
+                        stops: <double>[0.0, 1.0],
+                        tileMode: TileMode.clamp),
+                  ),
+                  width: 100.0,
+                  height: 1.0,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                  child: Text(
+                    'Botybuy',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontFamily: 'WorkSansMedium'),
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: <Color>[
+                          Colors.white,
+                          Colors.white10,
+                        ],
+                        begin: FractionalOffset(0.0, 0.0),
+                        end: FractionalOffset(1.0, 1.0),
+                        stops: <double>[0.0, 1.0],
+                        tileMode: TileMode.clamp),
+                  ),
+                  width: 100.0,
+                  height: 1.0,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
